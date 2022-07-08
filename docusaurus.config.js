6b0ccf394619f6fba65c093bb29bf60f624c5d72
an-lee <an.lee.work@gmail.com>
2022-07-08 11:45:19 +0800

    fix config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/baizhiheizi/docs.batata.im/tree/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -58,7 +58,7 @@ const config = {
         },
         items: [
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/baizhiheizi/docs.batata.im",
             label: "GitHub",
             position: "right",
           },
@@ -66,47 +66,6 @@ const config = {
       },
       footer: {
         style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "How to Connect",
-                to: "/category/how-to-connect",
-              },
-              {
-                label: "How to deposit",
-                to: "/category/how-to-deposit",
-              },
-              {
-                label: "How to pay",
-                to: "/category/how-to-pay",
-              },
-              {
-                label: "How to withdraw",
-                to: "/category/how-to-withdraw",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Twitter",
-                href: "https://twitter.com/batata_web3",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "GitHub",
-                href: "https://github.com/baizhiheizi/prsdigg",
-              },
-            ],
-          },
-        ],
         copyright: `Copyright © ${new Date().getFullYear()} Batata, Inc. Built with Docusaurus.`,
       },
       prism: {
